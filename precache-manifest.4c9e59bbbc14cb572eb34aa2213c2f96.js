a705ddba817af6a64b4ec75ca89d63ca9d4979e0
Denmark-banana <neat507kr@gmail.com>
2020-04-11 15:12:28 +0900

    Updates

---
+++ precache-manifest.4c9e59bbbc14cb572eb34aa2213c2f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b92b63f48c0ebe0997ea7d7f8fcdd78",
+    "url": "/venus/index.html"
+  },
+  {
+    "revision": "9e422ea6d41fda103422",
+    "url": "/venus/static/js/2.a766d44d.chunk.js"
+  },
+  {
+    "revision": "ff2a223b51b86718de583271daa0b126",
+    "url": "/venus/static/js/2.a766d44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46194e84022a32353aa1",
+    "url": "/venus/static/js/main.3efbc246.chunk.js"
+  },
+  {
+    "revision": "d197dff84a739468d82b",
+    "url": "/venus/static/js/runtime-main.e7444bb6.js"
+  },
+  {
+    "revision": "f928ac6b82e7fab04d74ebdc9dba38d3",
+    "url": "/venus/static/media/no_poster.f928ac6b.png"
+  }
+]);
